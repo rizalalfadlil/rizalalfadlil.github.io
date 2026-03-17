c04c8998048baae916f07cc034617614670e9a0f
kevin <kevinsurya0407@gmail.com>
2026-03-17 17:48:06 +0700

    change some text

---
+++ data/projects.js
@@ -0,0 +1,93 @@
+export const projects = [
+    {
+        description:
+            "Selamat datang di Fita Pro, aplikasi jurnal digital yang dikelola oleh Akademi Farmasi YPF, yang dirancang untuk memudahkan akademisi, peneliti, dan mahasiswa dalam mengakses dan berbagi informasi terkini seputar ilmu farmasi.",
+        imageUrl: "./projectsThumbnail/fitapro.png",
+        url: "https://fitapro.akfarypf.ac.id/",
+        category: "client",
+    },
+    {
+        description: "gallery app created with laravel",
+        imageUrl: "./projectsThumbnail/gallery-laravel.png",
+        sourceCode: "https://github.com/rizalalfadlil/laravel-gallery",
+        category: "personal",
+    },
+    {
+        description: "people/characters crud api with laravel",
+        imageUrl: "./projectsThumbnail/api.png",
+        sourceCode: "https://github.com/rizalalfadlil/characters-api",
+        category: "personal",
+    },
+    {
+        description:
+            "Coffee ordering app created as part of a competency test (UKK) and a graduation requirement.",
+        imageUrl: "./projectsThumbnail/ngopi.png",
+        url: "https://bisa-ngopi.vercel.app/",
+        sourceCode: "https://github.com/rizalalfadlil/next-test",
+        category: "academic",
+    },
+    {
+        description:
+            "online hotel reservation app developed for pre-competency test practice.",
+        imageUrl: "./projectsThumbnail/hotel.png",
+        sourceCode: "https://github.com/rizalalfadlil/hotel-hebat",
+        category: "academic",
+    },
+    {
+        description:
+            "An online auction platform designed for transparent and fair bidding, also created as a practice project before the competency test.",
+        imageUrl: "./projectsThumbnail/lelang.png",
+        sourceCode: "https://github.com/rizalalfadlil/lelang-online-backend",
+        category: "academic",
+    },
+    {
+        description:
+            "A chatbot application with AI using gemini api that simulates conversations with characters from popular games.",
+        imageUrl: "./projectsThumbnail/halu.png",
+        category: "personal",
+    },
+    {
+        description:
+            "Undefeatable Tic Tac Toe This app takes the timeless game of Tic Tac Toe and elevates it with an unbeatable AI opponent.",
+        imageUrl: "./projectsThumbnail/ttt.png",
+        url: "https://rizalalfadlil.github.io/undefeatable-tictactoe//",
+        sourceCode: "https://github.com/rizalalfadlil/undefeatable-tictactoe",
+        category: "personal",
+    },
+    {
+        description:
+            "a online examination platform developed during an internship period as a product outcome.",
+        imageUrl: "./projectsThumbnail/ujian.png",
+        url: "https://bisa-ngopi.vercel.app/",
+        sourceCode: "https://github.com/rizalalfadlil/aplikasi",
+        category: "academic",
+    },
+    {
+        description: "Generate anagram from a person name.",
+        imageUrl: "./projectsThumbnail/anagram.png",
+        url: "https://anagram-nama.pages.dev/",
+        sourceCode: "https://github.com/rizalalfadlil/anagram",
+        category: "personal",
+    },
+    {
+        description: "Personal blog platform",
+        imageUrl: "./projectsThumbnail/blog.png",
+        url: "https://blog-rizalalfadlil.vercel.app/",
+        sourceCode: "https://github.com/rizalalfadlil/blog-2",
+        category: "personal",
+    },
+    {
+        description: "random percentage generator",
+        imageUrl: "./projectsThumbnail/bit.png",
+        url: "https://rizalalfadlil.github.io/bit/",
+        sourceCode: "https://github.com/rizalalfadlil/bit",
+        category: "personal",
+    },
+    {
+        description: "random percentage maker as requested by tes karbit user",
+        imageUrl: "./projectsThumbnail/tes-maker.png",
+        url: "https://persen.ism-smile.web.id/",
+        sourceCode: "https://github.com/rizalalfadlil/anything",
+        category: "personal",
+    },
+];

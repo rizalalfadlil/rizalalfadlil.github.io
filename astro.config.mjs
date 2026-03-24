8b3d93bf54337e1557e3ee77abdda5de05ce38c3
Hafidz Rizal Al-Fadlil <115386426+rizalalfadlil@users.noreply.github.com>
2026-03-25 06:27:31 +0700

    Set site URL in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://rizalalfadlil.github.io',
   vite: {
     plugins: [tailwindcss()],
   },
